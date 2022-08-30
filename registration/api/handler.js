68991e42c4cc9fe1e899c225d2873c16d93dbecb
Samantha Piatt <samantha.piatt@childrens.harvard.edu>
2022-08-30 16:09:48 -0400

    Add user email check before insert.

--- registration/api/handler.js
+++ registration/api/handler.js
@@ -5,47 +5,73 @@ const AWS = require('aws-sdk');
 AWS.config.update({ region: process.env.REGION || 'us-east-1' });
 const DDB = new AWS.DynamoDB.DocumentClient();
 
-const status = { OK: 200, SERVER_ERROR: 500 };
-const headers = { 'Content-Type': 'application/json' };
+const status = { 
+  OK: 200,
+  BAD_REQUEST: 400,
+  SERVER_ERROR: 500
+};
 
-module.exports.registerUser = function(event) {
+class Response {
+  constructor(message = "", statusCode = status.OK){
+    const body = JSON.stringify({ message });
+    this.statusCode = statusCode;
+    this.body = body;
+    this.headers = { 'Content-Type': 'application/json' };
+  }
+}
+
+module.exports.registerUser = async function(event) {
   const { firstName, lastName, email } = JSON.parse(event.body);
   const uid = uuid();
   const createdAt = new Date().toISOString();
 
-  return DDB.put({
-    TableName: process.env.USER_TABLE,
-    Item: {
-      uid,
-      applyReason: process.env.USER_REASON,
-      authenticationProviderId: process.env.USER_POOL,
-      createdAt,
-      createdBy: '_system_',
-      email,
-      username: email,
-      encryptedCreds: 'N/A',
-      firstName,
-      lastName,
-      identityProviderName: process.env.IDP_NAME,
-      isAdmin: false,
-      isExternalUser: true,
-      isNativePoolUser: false,
-      isSamlAuthenticatedUser: true,
-      ns: `${process.env.IDP_NAME}||||${process.env.USER_POOL}`,
-      projectId: [],
-      rev: '1',
-      status: 'pending',
-      userRole: process.env.USER_ROLE
+  try {
+    const emailExists = await DDB.query({
+      TableName: process.env.USER_TABLE,
+      IndexName: 'Principal',
+      KeyConditionExpression: 'username = :email',
+      ExpressionAttributeValues: { ':email': email },
+      Select: 'COUNT'
+     }).promise()
+      .then(({ Count }) => Count > 0);
+     
+    if(emailExists){
+      const message = `User email ${email} already exists.`;
+      console.error(message);
+      return new Response(message, status.BAD_REQUEST);
     }
-  }).promise()
-    .then(() => {
-      const message = `Successfully registered user ${uid}.`;
-      console.log(message);
-      return { headers, statusCode: status.OK, body: JSON.stringify({ message }) };
-    })
-    .catch(error => {
-      const message = 'An error occured while registering user.';
-      console.error(message, error.message);
-      throw new Error(`[${status.SERVER_ERROR}] ${message}`);
-    });
+
+    await DDB.put({
+      TableName: process.env.USER_TABLE,
+      Item: {
+        uid,
+        applyReason: process.env.USER_REASON,
+        authenticationProviderId: process.env.USER_POOL,
+        createdAt,
+        createdBy: '_system_',
+        email,
+        username: email,
+        encryptedCreds: 'N/A',
+        firstName,
+        lastName,
+        identityProviderName: process.env.IDP_NAME,
+        isAdmin: false,
+        isExternalUser: true,
+        isNativePoolUser: false,
+        isSamlAuthenticatedUser: true,
+        ns: `${process.env.IDP_NAME}||||${process.env.USER_POOL}`,
+        projectId: [],
+        rev: '1',
+        status: 'pending',
+        userRole: process.env.USER_ROLE
+      }
+    }).promise();
+    const message = `Successfully registered user ${uid}.`;
+    console.log(message);
+    return new Response(message);
+  } catch(error) {
+    const message = 'An error occured while registering user.';
+    console.error(message, error.message);
+    return new Response(message, status.SERVER_ERROR);
+  }
 }
--- registration/api/handler.test.js
+++ registration/api/handler.test.js
@@ -2,14 +2,10 @@ const AWS = require('aws-sdk');
 const uuid = require('uuid');
 const { registerUser } = require('./handler');
 
-const putMock = jest.fn();
 jest.mock("aws-sdk");
-AWS.DynamoDB.DocumentClient.prototype.put.mockImplementation(params => ({ promise: () => putMock(params) }));
-
 jest.mock('uuid/v4');
-
-const logger = {};
-const cbMock = jest.fn();
+const putMock = jest.fn();
+const queryMock = jest.fn();
 
 process.env.USER_TABLE = 'Users';
 process.env.USER_REASON = 'Testing';
@@ -17,75 +13,122 @@ process.env.USER_POOL = 'some pool id';
 process.env.IDP_NAME = 'Test IDP';
 
 describe('registerUser', () => {
+  const logger = {};
+  const testEmail = 'test@email.com';
   const testEvent = {
     body: JSON.stringify({
       firstName: 'Test',
       lastName: 'Tester',
-      email: 'test@email.com'
+      email: testEmail
     })
   };
   const testUuid = '12345';
   const databaseError = new Error('Some database error');
+  const userExists = { Count: 1 };
+  const userUndefined = { Count: 0 };
   
   beforeAll(() => {
     jest.useFakeTimers('modern');
     jest.setSystemTime(new Date(1466424490000));
   });
   beforeEach(() => {
+    AWS.DynamoDB.DocumentClient.prototype.put.mockImplementation(params => ({ promise: () => putMock(params) }));
+    AWS.DynamoDB.DocumentClient.prototype.query.mockImplementation(params => ({ promise: () => queryMock(params) }));
+    uuid.mockReturnValue(testUuid);
+
     // capture and print nothing
     logger.info = jest.spyOn(console, 'log').mockImplementation(() => {});
     logger.error = jest.spyOn(console, 'error').mockImplementation(() => {});
-
-    uuid.mockReturnValue(testUuid);
-    // uuidMock = jest.spyOn(uuid, 'v4').mockReturnValue(testUuid);
-
-    cbMock.mockImplementation((error, response) => ({ error, response }));
   });
 
-  it('should not throw an error', async () => {
-    putMock.mockResolvedValueOnce();
-    expect(registerUser(testEvent)).resolves.not.toThrow();
-  });
-  it('should attempt to add user to the database with correct params', async () => {
-    putMock.mockResolvedValueOnce();
-    await registerUser(testEvent);
-    expect(putMock).toHaveBeenCalled();
-    expect(putMock.mock.calls).toMatchSnapshot();
+  describe('user exists', () => {
+    it('should attempt to query user with correct params', async () => {
+      queryMock.mockResolvedValueOnce(userExists);
+      await registerUser(testEvent);
+      expect(queryMock).toHaveBeenCalled();
+      expect(queryMock.mock.calls).toMatchSnapshot();
+    });
+    it('should not try to add the user', async () => {
+      queryMock.mockResolvedValueOnce(userExists);
+      expect(putMock).not.toHaveBeenCalled();
+    });
+    it('should return correct response', async () => {
+      queryMock.mockResolvedValueOnce(userExists);
+      const response = await registerUser(testEvent);
+      expect(response).toMatchSnapshot();
+    });
+    it('should log an error message containing email', async () => {
+      queryMock.mockResolvedValueOnce(userExists);
+      await registerUser(testEvent);
+      expect(JSON.stringify(logger.error.mock.calls)).toMatchSnapshot();
+      expect(JSON.stringify(logger.error.mock.calls).includes(testEmail)).toBeTruthy();
+    });
+    it('should not throw an exception', async () => {
+      queryMock.mockResolvedValueOnce(userExists);
+      expect(registerUser(testEvent)).resolves.not.toThrow();
+    });
+    
+    it('should return correct response on database error', async () => {
+      queryMock.mockRejectedValueOnce(databaseError);
+      const response = await registerUser(testEvent);
+      expect(response).toMatchSnapshot();
+    });
+    it('should log error message on database error', async () => {
+      queryMock.mockRejectedValueOnce(databaseError);
+      await registerUser(testEvent);
+      expect(JSON.stringify(logger.error.mock.calls)).toMatchSnapshot();
+    });
+    it('should not throw an exception on database error', async () => {
+      queryMock.mockRejectedValueOnce(databaseError);
+      expect(registerUser(testEvent)).resolves.not.toThrow();
+    });
   });
 
-  it('should return status 200 on success', async () => {
-    putMock.mockResolvedValueOnce();
-    const response = await registerUser(testEvent);
-    expect(response.statusCode).toEqual(200);
-  });
-  it('should return a message on success', async () => {
-    putMock.mockResolvedValueOnce();
-    const response = await registerUser(testEvent);
-    expect(response.body).toMatchSnapshot();
-  });
-  it('should throw an error on db error', async () => {
-    putMock.mockRejectedValueOnce(databaseError);
-    expect(registerUser(testEvent)).rejects.toThrow();
-  });
+  describe('user does not exist', () => {
+    beforeEach(() => {
+      queryMock.mockResolvedValueOnce(userUndefined);
+    });
+    it('should attempt to add user to the database with correct params', async () => {
+      putMock.mockResolvedValueOnce();
+      await registerUser(testEvent);
+      console.log('DEBUG', putMock.mock.calls);
+      expect(putMock).toHaveBeenCalled();
+      expect(putMock.mock.calls).toMatchSnapshot();
+    });
+    it('should return correct response on success', async () => {
+      putMock.mockResolvedValueOnce();
+      const response = await registerUser(testEvent);
+      expect(response).toMatchSnapshot();
+    });
+    it('should log the user id on success', async () => {
+      putMock.mockResolvedValueOnce();
+      await registerUser(testEvent);
+      expect(JSON.stringify(logger.info.mock.calls)).toMatchSnapshot();
+      expect(JSON.stringify(logger.info.mock.calls).includes(testUuid)).toBeTruthy();
+    });
+    it('should not throw an exception on success', async () => {
+      putMock.mockResolvedValueOnce();
+      expect(registerUser(testEvent)).resolves.not.toThrow();
+    });
 
-  // Logging
-  it('should log the user id that was successfull created', async () => {
-    putMock.mockResolvedValueOnce();
-    await registerUser(testEvent);
-    expect(JSON.stringify(logger.info.mock.calls).includes(testUuid)).toBeTruthy();
-    expect(JSON.stringify(logger.info.mock.calls)).toMatchSnapshot();
+    it('should return correct response on database error', async () => {
+      putMock.mockRejectedValueOnce(databaseError);
+      const response = await registerUser(testEvent);
+      expect(response).toMatchSnapshot();
+    });
+    it('should log error message on database error', async () => {
+      putMock.mockRejectedValueOnce(databaseError);
+      await registerUser(testEvent);
+      expect(JSON.stringify(logger.error.mock.calls)).toMatchSnapshot();
+    });
+    it('should not throw an exception on database error', async () => {
+      putMock.mockRejectedValueOnce(databaseError);
+      expect(registerUser(testEvent)).resolves.not.toThrow();
+    });
   });
-  it('should log error message on database failure', async () => {
-    putMock.mockRejectedValueOnce(databaseError);
-    try { await registerUser(testEvent); }
-    catch (e) {}
-    expect(JSON.stringify(logger.error.mock.calls)).toMatchSnapshot();
-  });
-
-  // it('should try to get the database parameters with the correct input arguments')
 
   afterEach(() => {
-    jest.clearAllMocks();
+    jest.resetAllMocks();
   });
   afterAll(() => {
     jest.useRealTimers();
